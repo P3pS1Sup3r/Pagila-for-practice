3f84429c355a6da2dd01f8a243a6bc79f740122d
mapk <shark-tok@mail.ru>
2026-01-17 21:51:06 +0500

    add custom scheme and data for recursive sql

---
+++ custom-schema.sql
@@ -0,0 +1,13 @@
+CREATE SCHEMA recursive;
+
+CREATE TABLE recursive.employees (
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    position VARCHAR(100) NOT NULL,
+    hire_date DATE NOT NULL,
+    salary NUMERIC(10, 2) NOT NULL,
+    department VARCHAR(50) NOT NULL,
+    manager_id INTEGER,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES recursive.employees(employee_id)
+);
